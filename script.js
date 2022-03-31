2d52577d0dfad1b2471cf0761bcd563e5be41156
Andrew <asjogren@leisdstudent.ws>
2022-03-31 10:10:13 -0500

    final

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function name() {
 }
 
 function nameresponse(y) {
-    alert(`Hello, ${y}`)
+    alert(`Hello, ${y}. This is a game of chance, you're trying to get closest to 20 without going over.`)
 }
 
 nameresponse(name())
@@ -14,20 +14,69 @@ function random() {
     return random
 }
 
+
+
 function user(x) {
   
-    do { 
-      var x = random()
-      var math1 = (math1 + x)
-      alert(`You drew ${x}. Your new total is ${math1}`)
-      console.log(x)  
-      user(random())
-    } while (math1 < 1)
+random()
   
+math1 = 0
+  
+  do {
+     m = random()  
+     math1 = (m + math1)
+     var answer = prompt(`You drew ${m} and your new total is ${math1}. "Y" to keep drawing, "N" to quit.`)  
+  } while (math1 <= 20 && answer == "Y" && answer != "N")
+  
+  return math1  
+     
 }
+
+user()
+
+alert(`Your total is ${math1}.`)
+
+
+
+function comp(c) {
   
-
-
-
-user(random())
+random()
   
+math2 = 0
+  
+  do {
+     v = random()  
+     math2 = (v + math2)
+     alert(`Computer drew ${v} and computers new total is ${math2}.`)  
+  } while (math2 <= 16)
+  
+  return math2   
+     
+}
+
+comp()
+
+alert(`Computers total is ${math2}.`)
+
+function comparing(x ,y) {
+  if (math1 > 20 && math2 > 20) {
+    alert(`Both of you lose!`)
+  }
+  
+  if (math1 > math2 && math2 < 20 && math1 > 20) {
+    alert(`Computer wins!`)
+  }
+  
+  if (math2 > math1 && math1 < 20 && math2 > 20) {
+    alert(`You win!`)
+  }
+  
+  if (math1 > math2 && math2 < 20 && math1 < 20) {
+    alert(`Computer wins!`)
+  }
+  
+  if (math2 > math1 && math1 < 20 && math2 < 20)
+    alert(`You win!`)
+}
+
+comparing()
